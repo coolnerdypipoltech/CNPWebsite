ad38a4d823edfaba12deceab1589cc6725c0a155
Dylan Ocampo <dylanomar@live.com.mx>
2025-12-01 22:24:35 -0600

    no router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import DinoGame from './pages/DinoGame'
-import Test from './pages/Test'
 import Main from './Main'
 
 export default function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/CNPWebsite/dinogame" element={<DinoGame />} />
-        <Route path="/CNPWebsite/test" element={<Test />} />
-      </Routes>
-    </Router>
+    <Main />
   )
 }
